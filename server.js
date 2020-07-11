bff86ce328beb7c0d3be6808d50570b646413562
Eigminas Slavinskas <e.slavinskas@newcastle.ac.uk>
2020-07-11 23:42:42 +0200

    Version 1.0.0

--- server.js
+++ server.js
@@ -6,6 +6,7 @@ const items = require('./routes/api/Items');
 
 const app = express();
 
+const path = require('path');
 // body parser middleware
 app.use(bodyParser.json());
 
@@ -19,6 +20,16 @@ mongoose.connect(db, {useNewUrlParser: true, useUnifiedTopology: true})
 
 // use routes
 app.use('/api/items', items);
+
+// Serve static assests if in production
+if(process.env.NODE_ENV === 'production') {
+    // set static folder
+    app.use(express.static('client/build'));
+
+    app.get('*', (req, res) => {
+        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+    });
+}
 
 const port = process.env.PORT || 5000;
 
